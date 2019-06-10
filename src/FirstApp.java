55d8512cb1214d440c4244b4c5f8f581fcb61260
cristinam <mateescu.cristina24@yahoo.ro>
2019-06-10 13:52:17 +0300

    initial commit

---
+++ src/FirstApp.java
@@ -0,0 +1,36 @@
+public class FirstApp {
+
+    public static void main(String[] args) {
+        //
+        byte b=120;
+        short c=128;
+        int i=10;
+        //float
+        long l=10000000L;
+        float f=10.3F;
+        double f1=10;
+        //characters
+        char c1='c';
+        char c2='\n';
+        char c3='\t'; // tab 5 spatii
+        char c4='\u00A9';
+        char c5='\r';
+        //BOOLEAN
+        boolean bool=true;
+        boolean bool_false = false;
+        boolean b3;
+        b3=false;
+        b3=true;
+        System.out.println("Number is:\t\n" +c4);
+
+        int y1=10;
+        int y2=11;
+        int y3=12;
+        int y4=13;
+        boolean b1 =y1++<y2-- && ++y2<y3--;  // 1.comparatia dupa incrementare   2. comparatia inainte de incrementare
+        //1. 10<11  si 12<11
+
+        System.out.println(b1);
+    }
+
+}
